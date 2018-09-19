11c2844bd5680d608593eb29bd5e4ffbb8ccbbc9
Zachary Cobb <zac.cobb9224@gmail.com>
2018-09-19 13:07:56 -0500

    Added comments.

--- StudentsAPI/Controllers/StudentController.cs
+++ StudentsAPI/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿//Zachary Cobb and Trent Bradburry
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using StudentsAPI.Models;
 
--- StudentsAPI/Models/DataLayer.cs
+++ StudentsAPI/Models/DataLayer.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿//Zachary Cobb and Trent Bradburry
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,6 +9,8 @@ namespace StudentsAPI.Models
     {
         private List<Student> _students = new List<Student>();
         string path = @"StudentData.csv";
+
+        //Loads csv into _students List
         public void LoadFile()
         {
             string[] lines = File.ReadAllLines(path);
@@ -15,10 +18,10 @@ namespace StudentsAPI.Models
             {
                 string[] value = lines[i].Split(",");
                 _students.Add(new Student { Id = value[0], Name = value[1], Gpa = float.Parse(value[2]) });
-
             }
         }
 
+        //Writes list of students to csv file
         public void SaveChanges()
         {
             string[] write = new string[_students.Count];
@@ -31,30 +34,35 @@ namespace StudentsAPI.Models
             File.WriteAllLines(path, write);
         }
 
+        //Returns the List of all students
         public List<Student> GetStudents()
         {
             return _students;
         }
 
+        //Returns a student in _students List using the given ID
         public Student GetStudentByID(string id)
         {
             return _students.Find(s => s.Id == id);
         }
 
+        //Adds a new student to _students List using attributes from given student
         public void CreateStudent(Student student)
         {
             _students.Add(new Student { Id = student.Id, Name = student.Name, Gpa = student.Gpa });
         }
 
+        //Removes given student from _students List
         public void Delete(Student student)
         {
             _students.Remove(student);
         }
 
+        //Returns the minimum and maximum GPA for all students in _students List
         public float[] Range()
         {
             float[] range = new float[]
-{
+            {
                 _students.Min(g => g.Gpa),
                 _students.Max(g => g.Gpa)
             };
